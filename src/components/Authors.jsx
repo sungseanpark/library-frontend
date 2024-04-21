921a7727488086e7049cbfb8cee63006901c8498
sungseanpark <sungseanpark@gmail.com>
2024-04-20 20:59:59 -0700

    Complete part 8.8

--- src/components/Authors.jsx
+++ src/components/Authors.jsx
@@ -1,8 +1,18 @@
+import { useQuery } from '@apollo/client'
+import { ALL_AUTHORS } from '../queries'
+
 const Authors = (props) => {
   if (!props.show) {
     return null
   }
-  const authors = []
+  const result = useQuery(ALL_AUTHORS)
+  
+  if (result.loading)  {
+    return <div>loading...</div>
+  }
+
+  const authors = result.data.allAuthors
+  // const authors = []
 
   return (
     <div>
--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.jsx";
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
+import { ApolloClient, InMemoryCache, gql, ApolloProvider } from '@apollo/client'
+
+const client = new ApolloClient({
+  uri: 'http://localhost:4000',
+  cache: new InMemoryCache(),
+})
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <ApolloProvider client={client}>
     <App />
-  </React.StrictMode>
-);
+  </ApolloProvider>
+)
